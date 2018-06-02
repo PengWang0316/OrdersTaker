4dcd92d40c23db983bae9dbca0b9104f9dc3aa98
Kevin W <rocwang0316@gmail.com>
2018-06-02 16:44:50 -0700

    More template files were added.

---
+++ app/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { AppContainer } from 'react-hot-loader';
+
+import App from './Components/App';
+import configureStore from './store/ConfigureStore';
+import './styles/index.global.css';
+import registerServiceWorker from './registerServiceWorker';
+
+// import jQuery from "jQuery";
+// require('bootstrap');
+const store = configureStore();
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('app')
+// );
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    </AppContainer>,
+    document.getElementById('app'),
+  );
+};
+render(App);
+
+// Webpack Hot Module Replacement API
+if (module.hot)
+  module.hot.accept('./Components/App', () => render(App));
+
+// Registering a service worker in the production enviroment.
+registerServiceWorker();
---
+++ app/service-worker.js
@@ -0,0 +1,138 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+// const workbox = require('workbox-sw');
+/**
+ * DO NOT EDIT THE FILE MANIFEST ENTRY
+ *
+ * The method precache() does the following:
+ * 1. Cache URLs in the manifest to a local cache.
+ * 2. When a network request is made for any of these URLs the response
+ *    will ALWAYS comes from the cache, NEVER the network.
+ * 3. When the service worker changes ONLY assets with a revision change are
+ *    updated, old cache entries are left as is.
+ *
+ * By changing the file manifest manually, your users may end up not receiving
+ * new versions of files because the revision hasn't changed.
+ *
+ * Please use workbox-build or some other tool / approach to generate the file
+ * manifest which accounts for changes to local files and update the revision
+ * accordingly.
+ */
+//
+// const workboxSW = new self.WorkboxSW({
+//   skipWaiting: true,
+//   clientsClaim: true
+// });
+// // The [] will be replaced:
+// workboxSW.precache([]);
+workbox.skipWaiting();
+workbox.clientsClaim();
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+// Custom code...
+
+
+
+
+
+/* ******************** Start notification related code ********************** */
+
+/** The method that uses to convert urlBase64 to Uint8Array.
+  * The code gets from web-push https://www.npmjs.com/package/web-push
+  * @param {string} base64String is a url base 64 string.
+  * @return {Uint8Array} Return a Uint8Array object.
+*/
+const urlBase64ToUint8Array = base64String => {
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/-/g, '+').replace(/_/g, '/');
+  const rawData = self.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  // console.log('uint8array:', outputArray);
+  return outputArray;
+};
+
+
+const NOTIFICATION_OPEN_URL = 'https://kairoscope.resonancepath.com/sharedReadings';
+const NOTIFICATION_TITLE = 'Order Taker';
+/** The method that start to subscribe a user with PushManager.
+  * @param {object} an object that contains user's id and jwt message value.
+  * @param {object} port represents the port that will be used sending message back to react code.
+  * @return {null} No return.
+*/
+const subscribeUser = ({ userId, jwtMessage }, port) => {
+  // console.log(self);
+  // Making sure the browser supports the PushManager.
+  if ('PushManager' in self) {
+    const subscribeOptions = {
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUint8Array('BCAjEL-OP1949tjluWWkRvLQsQ5eI_qkypEAuvixiwFFn1KPnBpT1KNyEopZg-WNANBR8GmM9pxAg2XhBotjtf0')
+    };
+    self.registration.pushManager.subscribe(subscribeOptions)
+      .then(pushSubscription => port.postMessage(JSON.stringify(pushSubscription)));
+  }
+};
+
+/* The listener that uses to listen the message that the client sends.
+Will use this listener to call the function in the service worker file.
+The style is evt.data = { methodName: method parameter}
+  * TODO It seems like the message will be received twice in Chrome (The first one just has object key and the second one has the full object content.). But FireFox is fine. No sure whether it is a bug in Chrome.
+*/
+self.addEventListener('message', ({ data, ports }) => {
+  const objectKeys = Object.keys(data);
+  if (!data[objectKeys[0]]) return; // If the message does not have any body, do not fire the subscriptUser call and return.
+  switch (objectKeys[0]) {
+    case 'subscribeUser':
+      subscribeUser(data[objectKeys[0]], ports[0]);
+      break;
+    default:
+      break;
+  }
+});
+
+/* notification listener method */
+self.addEventListener('push', event => {
+  if (event.data) {
+    self.registration.showNotification(NOTIFICATION_TITLE, {
+      body: event.data.text(),
+      icon: '/launcher-icon-web.png',
+      badge: '/badge-128x128.png',
+      // actions: [
+      //   {
+      //     action: 'view-reading-action',
+      //     title: 'View the reading'
+      //   }]
+    });
+  } else {
+    console.log('This push event has no data.');
+  }
+});
+
+/* Notification click listener */
+self.addEventListener('notificationclick', event => {
+  const clickedNotification = event.notification;
+  clickedNotification.close();
+
+  // Do something as the result of the notification click
+  const urlToOpen = new URL(NOTIFICATION_OPEN_URL, self.location.origin).href;
+  // If has already had a browser window open with the same url, use it instead of open a new one.
+  const promiseChain = self.clients.matchAll({
+    type: 'window',
+    includeUncontrolled: true
+  }).then((windowClients) => {
+    let matchingClient = null;
+
+    for (let i = 0; i < windowClients.length; i++) {
+      const windowClient = windowClients[i];
+      if (windowClient.url === urlToOpen) {
+        matchingClient = windowClient;
+        break;
+      }
+    }
+    if (matchingClient) return matchingClient.focus();
+    return self.clients.openWindow(urlToOpen);
+  });
+  event.waitUntil(promiseChain);
+});
