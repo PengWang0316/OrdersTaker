03c0657bbe02bba9ecb91d7bc1d3a701c285e842
Kevin W <rocwang0316@gmail.com>
2018-06-02 19:34:11 -0700

    Index reducer was created.

---
+++ app/reducers/Index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+// import { routerReducer } from 'react-router-redux';
+
+
+
+// import bigrams from '../reducers/BigramsReducers';
+
+export default combineReducers({
+  
+});
