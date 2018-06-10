36b17477aded04330ff34f491c29780359209bb4
Kevin W <rocwang0316@gmail.com>
2018-06-09 22:55:56 -0700

    Added MenuCategory in the HomePageCOntainer.

--- app/components/containers/HomePageContainer.js
+++ app/components/containers/HomePageContainer.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 
 import Banner from '../Banner';
+import MenuCategory from '../MenuCategory';
 
 export const HomePage = () => (
   <Fragment>
     <Banner />
+    <MenuCategory />
   </Fragment>
 );
 export default HomePage;
