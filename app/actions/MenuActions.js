a7d157f2db426a8e2460e5248bb6d52eb586859a
Kevin W <rocwang0316@gmail.com>
2018-06-11 17:13:49 -0700

    Added the MenuActions.js file.

---
+++ app/actions/MenuActions.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+import { API_FETCH_ALL_MENU } from './ApiUrls';
+import { FETCH_ALL_MENU_SUCCESS, FETCH_ALL_MENU_ITEM_SUCCESS } from './ActionTypes';
+
+const fetchAllMenuSuccess = data => ({
+  type: FETCH_ALL_MENU_SUCCESS,
+  menus: data
+});
+
+/**
+ * Put the menu items to an object
+ * @param {array} data is an array that contains all menu information.
+ * @return {object} Return all menu item. { id: xxx, { otherInformationA: xxx, ... }j, ... }
+ * */
+const fetchAllMenuItemSuccess = data => {
+  const menuItems = {};
+  data.forEach(element => element.items.forEach(item => { menuItems[item.id] = item; }));
+  return {
+    type: FETCH_ALL_MENU_ITEM_SUCCESS,
+    menuItems
+  };
+};
+
+export const fetchAllMenu = () =>
+  dispatch => axios.get(API_FETCH_ALL_MENU)
+    .then(({ data }) => {
+      dispatch(fetchAllMenuSuccess(data));
+      dispatch(fetchAllMenuItemSuccess(data));
+    }).catch(err => console.error(err));
+
+export default fetchAllMenu;
