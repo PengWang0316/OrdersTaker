bfb3d06a9db17244cc7f910c10df181d0959cd49
Kevin W <rocwang0316@gmail.com>
2018-06-13 16:12:43 -0700

    Added the HomePageContainer component.

--- app/components/containers/HomePageContainer.js
+++ app/components/containers/HomePageContainer.js
@@ -1,12 +1,50 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { fetchAllMenu } from '../../actions/MenuActions';
 
 import Banner from '../Banner';
 import MenuCategory from '../MenuCategory';
 
-export const HomePage = () => (
-  <Fragment>
-    <Banner />
-    <MenuCategory />
-  </Fragment>
-);
-export default HomePage;
+/** The component that shows the home page containt */
+export class HomePageContainer extends Component {
+  static propTypes = {
+    menus: PropTypes.array,
+    fetchAllMenu: PropTypes.func.isRequired
+  };
+  static defaultProps = { menus: null };
+
+  /**
+   * call fetchAllMenu method when the menus is null
+   * @param {object} props is an object that contains props' values
+   */
+  constructor(props) {
+    super(props);
+    if (!props.menus) props.fetchAllMenu();
+  }
+
+  /**
+   * Rendering the jsx for the component.
+   * @return {jsx} Return jsx
+   */
+  render() {
+    const { menus } = this.props;
+    return (
+      <Fragment>
+        <Banner />
+        {menus && menus.map(menu => <MenuCategory menu={menu} key={menu._id} />)}
+      </Fragment>
+    );
+  }
+}
+
+/* istanbul ignore next */
+const mapStateToProps = state => ({
+  menus: state.menus
+});
+/* istanbul ignore next */
+const mapDispatchToProps = dispatch => ({
+  fetchAllMenu: () => dispatch(fetchAllMenu())
+});
+export default connect(mapStateToProps, mapDispatchToProps)(HomePageContainer);
