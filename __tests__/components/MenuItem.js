c1ec548fe1f9497fd343eb792581c0d5c44dbe36
Kevin W <rocwang0316@gmail.com>
2018-06-17 15:06:42 -0700

    Added the test code for thet MenuItem component.

--- __tests__/components/MenuItem.js
+++ __tests__/components/MenuItem.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { mount } from 'enzyme'; // Have to use mount to test a context.
 
 import { MenuItem } from '../../app/components/MenuItem';
+import context from '../../app/contexts/ShowDetailDialogContextTestHelper';
 
 jest.mock('@material-ui/core/Typography', () => 'Typography');
 jest.mock('@material-ui/core/Avatar', () => 'Avatar');
 jest.mock('../../app/components/PriceItem', () => 'PriceItem');
+jest.mock('../../app/contexts/ShowDetailDialogContext'); // The __mocks__/ShowDetailDialogContext.js will be used automatically.
+
+window.console.error = jest.fn(); // Silencing the unuseful warning causes by mount.
 
 describe('MenuItem', () => {
-  test('Snapshot', () => expect(renderer.create(<MenuItem {...{ handleClick: () => {}, item: { photo: 'photo', name: 'name' }, classes: { menuItem: 'menuItem', avatar: 'avatar', menuName: 'menuName' } }} />).toJSON())
+  const defaultProps = { item: { photo: 'photo', name: 'name' }, classes: { menuItem: 'menuItem', avatar: 'avatar', menuName: 'menuName' } };
+
+  test('Snapshot', () => expect(renderer.create(<MenuItem {...defaultProps} />).toJSON())
     .toMatchSnapshot());
+
+  test('Click avatar and Typography element', () => {
+    // const ComponentWithContext = getComponentWithContext();
+    // showDetailDialogContext.
+    const component = mount(<MenuItem {...defaultProps} />);
+    // console.log(component.html());
+    // console.log(component.find('Avatar'));
+    component.find('Avatar').simulate('click');
+    expect(context).toHaveBeenCalledTimes(1);
+    component.find('Typography').simulate('click');
+    expect(context).toHaveBeenCalledTimes(2);
+  });
 });
