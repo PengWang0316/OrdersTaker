12ce366372d1f2cb69126cf3d03ecf1dfdfc8910
Kevin W <rocwang0316@gmail.com>
2018-06-25 13:16:36 -0700

    Added the LoginRedirect component.

---
+++ __tests__/components/LoginRedirect.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+
+import { LoginRedirect } from '../../app/components/LoginRedirect';
+import { JWT_MESSAGE, LOGIN_CALLBACK_URL } from '../../app/config';
+
+jest.mock('@material-ui/core/LinearProgress', () => 'LinearProgress');
+jest.mock('@material-ui/core/Typography', () => 'Typography');
+
+describe('LoginRedirect', () => {
+  const defaultProps = {
+    classes: {
+      root: 'root',
+      progress: 'progeress',
+      title: 'title'
+    },
+    parserUserFromJwt: jest.fn(),
+    history: {
+      push: jest.fn()
+    },
+    location: { search: { match: jest.fn() } }
+  };
+  const getShallowComponent = (props = defaultProps) => shallow(<LoginRedirect {...defaultProps} />);
+
+  test('constructor without jwt', () => {
+    getShallowComponent();
+    expect(defaultProps.location.search.match).toHaveBeenCalledTimes(1);
+    expect(defaultProps.parserUserFromJwt).not.toHaveBeenCalled();
+    expect(localStorage.setItem).not.toHaveBeenCalled();
+    expect(localStorage.getItem).not.toHaveBeenCalled();
+    expect(defaultProps.history.push).not.toHaveBeenCalled();
+  });
+
+  test('constructor with jwt and user', () => {
+    getShallowComponent({ ...defaultProps, user: {} });
+    expect(defaultProps.location.search.match).toHaveBeenCalledTimes(2);
+    expect(defaultProps.parserUserFromJwt).not.toHaveBeenCalled();
+    expect(localStorage.setItem).not.toHaveBeenCalled();
+    expect(localStorage.getItem).not.toHaveBeenCalled();
+    expect(defaultProps.history.push).not.toHaveBeenCalled();
+  });
+
+  test('constructor with jwt and  without user', () => {
+    defaultProps.location.search.match.mockReturnValueOnce(['', 'jwtMessage']);
+    getShallowComponent();
+    expect(defaultProps.location.search.match).toHaveBeenCalledTimes(3);
+    expect(defaultProps.parserUserFromJwt).toHaveBeenCalledTimes(1);
+    expect(defaultProps.parserUserFromJwt).toHaveBeenLastCalledWith('jwtMessage');
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenLastCalledWith(JWT_MESSAGE, 'jwtMessage');
+    expect(localStorage.getItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.getItem).toHaveBeenLastCalledWith(LOGIN_CALLBACK_URL);
+    expect(defaultProps.history.push).toHaveBeenCalledTimes(1);
+    expect(defaultProps.history.push).toHaveBeenLastCalledWith('/');
+  });
+
+  test('constructor with jwt and  without user', () => {
+    defaultProps.location.search.match.mockReturnValueOnce(['', 'jwtMessage']);
+    localStorage.__STORE__[LOGIN_CALLBACK_URL] = 'callbackUrl'; // Set up a fake local storage value.
+    getShallowComponent();
+    expect(defaultProps.location.search.match).toHaveBeenCalledTimes(4);
+    expect(defaultProps.parserUserFromJwt).toHaveBeenCalledTimes(2);
+    expect(defaultProps.parserUserFromJwt).toHaveBeenLastCalledWith('jwtMessage');
+    expect(localStorage.setItem).toHaveBeenCalledTimes(2);
+    expect(localStorage.setItem).toHaveBeenLastCalledWith(JWT_MESSAGE, 'jwtMessage');
+    expect(localStorage.getItem).toHaveBeenCalledTimes(2);
+    expect(localStorage.getItem).toHaveBeenLastCalledWith(LOGIN_CALLBACK_URL);
+    expect(defaultProps.history.push).toHaveBeenCalledTimes(2);
+    expect(defaultProps.history.push).toHaveBeenLastCalledWith('callbackUrl');
+  });
+
+  test('snapshot', () => expect(renderer.create(<LoginRedirect {...defaultProps} />).toJSON()).toMatchSnapshot());
+});
