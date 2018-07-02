09df9705b951c7c7750eb4dff6f6a9d998330ebf
Kevin W <rocwang0316@gmail.com>
2018-07-01 23:02:02 -0700

    Changed the OrderFloatingButton to user orders.qty to show the quantity of the items.

--- app/components/OrderFloatingButton.js
+++ app/components/OrderFloatingButton.js
@@ -35,7 +35,7 @@ export const OrderFloatingButton = ({ orders, classes }) => {
     <div className={classes.root}>
       <Button variant="fab" className={classes.floatingButton} color="primary">
         <RoomServiceIcon />
-        <Badge badgeContent={Object.keys(orders).length} color="error" className={classes.badge} />
+        <Badge badgeContent={orders.qty} color="error" className={classes.badge} />
       </Button>
     </div>
   );
