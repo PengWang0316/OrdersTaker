a5b4e98b49309a125a375eff554643e4a969bb47
Kevin W <rocwang0316@gmail.com>
2018-07-02 23:07:01 -0700

    Added the OrdersReducers file.

---
+++ __tests__/reducers/OrdersReducers.js
@@ -0,0 +1,26 @@
+import { ADD_ORDER_SUCCESS } from '../../app/actions/ActionTypes';
+import orders from '../../app/reducers/OrdersReducers';
+
+describe('OrdersReducers', () => {
+  test('orders default return', () => expect(orders({ qty: 0 }, {})).toEqual({ qty: 0 }));
+  test('orders ADD_ORDER_SUCCESS without item._id', () => {
+    const item = { _id: 'itemId', other: 'other' };
+    const state = { qty: 0 };
+    expect(orders(state, { type: ADD_ORDER_SUCCESS, item, priceKey: 'priceKey' }))
+      .toEqual({ itemId: { ...item, qty: { priceKey: 1 } }, qty: 1 });
+  });
+
+  test('orders ADD_ORDER_SUCCESS without item._id and no priceKey yet', () => {
+    const item = { _id: 'itemId', other: 'other' };
+    const state = { itemId: { qty: {} }, qty: 0 };
+    expect(orders(state, { type: ADD_ORDER_SUCCESS, item, priceKey: 'priceKey' }))
+      .toEqual({ itemId: { ...item, qty: { priceKey: 1 } }, qty: 1 });
+  });
+
+  test('orders ADD_ORDER_SUCCESS without item._id and has priceKey', () => {
+    const item = { _id: 'itemId', other: 'other' };
+    const state = { itemId: { qty: { priceKey: 1 } }, qty: 0 };
+    expect(orders(state, { type: ADD_ORDER_SUCCESS, item, priceKey: 'priceKey' }))
+      .toEqual({ itemId: { ...item, qty: { priceKey: 2 } }, qty: 1 });
+  });
+});
---
+++ app/reducers/OrdersReducers.js
@@ -0,0 +1,21 @@
+import { ADD_ORDER_SUCCESS } from '../actions/ActionTypes';
+
+const orders = (state = { qty: 0 }, { type, item, priceKey }) => {
+  switch (type) {
+    case ADD_ORDER_SUCCESS: {
+      const returnState = { // Making a new object
+        ...state, // Copy the original state
+        qty: state.qty + 1 // Add one quantity
+      };
+      if (!returnState[item._id]) returnState[item._id] = { ...item, qty: { [priceKey]: 1 } }; // If the orders object has not had this item, directly add it in.
+      else returnState[item._id] = { // If the item has already been the orders object, copy the item and qty attribute in the item. Add one or give it a inital value.
+        ...item,
+        qty: { ...returnState[item._id].qty, [priceKey]: returnState[item._id].qty[priceKey] ? returnState[item._id].qty[priceKey] + 1 : 1 }
+      };
+      return returnState;
+    }
+    default:
+      return state;
+  }
+};
+export default orders;
