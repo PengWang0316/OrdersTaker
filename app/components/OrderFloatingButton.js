a615f92d3cf11e755351257362e9671512aa8877
Kevin W <rocwang0316@gmail.com>
2018-07-03 22:54:09 -0700

    Added a id for the Badge element.

--- app/components/OrderFloatingButton.js
+++ app/components/OrderFloatingButton.js
@@ -35,7 +35,7 @@ export const OrderFloatingButton = ({ orders, classes }) => {
     <div className={classes.root}>
       <Button id="orderFloatingButton" variant="fab" className={classes.floatingButton} color="primary">
         <RoomServiceIcon />
-        <Badge badgeContent={orders.qty} color="error" className={classes.badge}><span /></Badge>
+        <Badge id="orderNumberBadge" badgeContent={orders.qty} color="error" className={classes.badge}><span /></Badge>
       </Button>
     </div>
   );
