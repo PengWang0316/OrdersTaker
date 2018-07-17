9ddb79d07c711fddda8175d4ac08e500b81b40e7
Kevin W <rocwang0316@gmail.com>
2018-07-16 18:19:50 -0700

    Switched login check and login dialog control functionality from the Navbar component to the App component in order to give other components the access to the login dialog via a new LoginDialogContext.

--- __tests__/components/App.js
+++ __tests__/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
-import App from '../../app/components/App';
+import { App } from '../../app/components/App';
+import { JWT_MESSAGE } from '../../app/config';
 
 jest.mock('react-router-dom', () => ({
   BrowserRouter: ({ children }) => children,
@@ -13,9 +15,75 @@ jest.mock('react-router-dom', () => ({
 }));
 jest.mock('../../app/components/Navbar', () => 'Navbar');
 jest.mock('../../app/components/containers/HomePageContainer', () => 'HomePageContainer');
+jest.mock('../../app/components/LoginDialog', () => 'LoginDialog');
+jest.mock('../../app/components/Snackbars/LoginDialogSnackbar', () => 'LoginDialogSnackbar');
+jest.mock('../../app/components/Snackbars/LogoutSnackbar', () => 'LogoutSnackbar');
 
 window.console.log = jest.fn(); // Silencing the console log.
 
 describe('App component test', () => {
-  test('App snapshot', () => expect(renderer.create(<App />).toJSON()).toMatchSnapshot());
+  const defaultProps = {
+    user: { _id: 'userId' },
+    parserUserFromJwt: jest.fn()
+  };
+  const getShallowComponent = (props = defaultProps) => shallow(<App {...props} />);
+
+  test('Constructor and initial states', () => {
+    const { isLoginDialogOpen, isLogoutSnackBarOpen, isLoginSnackbarOpen, snackbarMessage } = getShallowComponent().state();
+    expect(isLoginDialogOpen).toBe(false);
+    expect(isLogoutSnackBarOpen).toBe(false);
+    expect(isLoginSnackbarOpen).toBe(false);
+    expect(snackbarMessage).toBe('');
+    expect(localStorage.getItem).not.toHaveBeenCalled();
+    expect(defaultProps.parserUserFromJwt).not.toHaveBeenCalled();
+
+    getShallowComponent({ ...defaultProps, user: {} });
+    expect(localStorage.getItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.getItem).toHaveBeenLastCalledWith(JWT_MESSAGE);
+    expect(defaultProps.parserUserFromJwt).not.toHaveBeenCalled();
+
+    localStorage.__STORE__[JWT_MESSAGE] = 'message'; // Set up a fake local storage value.
+    getShallowComponent({ ...defaultProps, user: {} });
+    expect(localStorage.getItem).toHaveBeenCalledTimes(2);
+    expect(defaultProps.parserUserFromJwt).toHaveBeenCalledTimes(1);
+    expect(defaultProps.parserUserFromJwt).toHaveBeenLastCalledWith('message');
+  });
+
+  test('handleToggleDialog', () => {
+    const component = getShallowComponent();
+    expect(component.state('isLoginDialogOpen')).toBe(false);
+    component.instance().handleToggleDialog();
+    expect(component.state('isLoginDialogOpen')).toBe(true);
+  });
+
+  test('handleToggleSnackbar', () => {
+    const component = getShallowComponent();
+    expect(component.state('isLoginSnackbarOpen')).toBe(false);
+    expect(component.state('snackbarMessage')).toBe('');
+
+    component.instance().handleToggleSnackbar({});
+    expect(component.state('isLoginSnackbarOpen')).toBe(true);
+    expect(component.state('snackbarMessage')).toBe('');
+
+    component.instance().handleToggleSnackbar('New message');
+    expect(component.state('isLoginSnackbarOpen')).toBe(false);
+    expect(component.state('snackbarMessage')).toBe('New message');
+  });
+
+  test('handleToggleLogoutSnackbar', () => {
+    const component = getShallowComponent();
+    expect(component.state('isLogoutSnackBarOpen')).toBe(false);
+    component.instance().handleToggleLogoutSnackbar();
+    expect(component.state('isLogoutSnackBarOpen')).toBe(true);
+  });
+
+  test('handleLogoutAction', () => {
+    const component = getShallowComponent();
+    component.instance().handleLogoutAction();
+    expect(component.state('isLoginDialogOpen')).toBe(false);
+    expect(component.state('isLogoutSnackBarOpen')).toBe(true);
+  });
+
+  test('App snapshot with user id', () => expect(renderer.create(<App {...defaultProps} />).toJSON()).toMatchSnapshot());
+  test('App snapshot without user id', () => expect(renderer.create(<App {...{ ...defaultProps, user: {} }} />).toJSON()).toMatchSnapshot());
 });
--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,7 @@
 
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 // import Loadable from 'react-loadable'; Does not work with react-hot-loader very well.
@@ -7,13 +9,19 @@ import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import importedComponent from 'react-imported-component';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
+import LoginDialog from './LoginDialog/';
+import LoginDialogSnackbar from './snackbars/LoginDialogSnackbar';
+import LogoutSnackbar from './snackbars/LogoutSnackbar';
+import Navbar from './Navbar';
+import { parserUserFromJwt } from '../actions/UserActions';
+import LoginDialogContext from '../contexts/LoginDialogContext';
+
+import { JWT_MESSAGE, HOME_PAGE_URL, LOGIN_REDIRECT_RUL, ORDER_PAGE_URL, ORDER_STATUS_PAGE_URL } from '../config';
+import LoadingAnimation from './SharedComponents/LoadingAnimation';
+
 // import { CSSTransitionGroup } from 'react-transition-group';
 
-import { HOME_PAGE_URL, LOGIN_REDIRECT_RUL, ORDER_PAGE_URL, ORDER_STATUS_PAGE_URL } from '../config';
-
-import Navbar from './Navbar';
 // import BackgroundMainPager from './BackgroundMainPager';
-import LoadingAnimation from './SharedComponents/LoadingAnimation';
 
 const theme = createMuiTheme({
   palette: {
@@ -44,24 +52,109 @@ const OrderPage = importedComponent(() => import(/* webpackChunkName: "OrderPage
 /* istanbul ignore next */
 const OrderStatusPage = importedComponent(() => import(/* webpackChunkName: "OrderStatusPageContainer" */ './containers/OrderStatusPageContainer').catch(err => console.log(err)), { LoadingComponent: LoadingAnimation });
 
-const App = props => (
-  <MuiThemeProvider theme={theme}>
-    <Router>
-      <div>
-        <CssBaseline />
-        <Navbar />
-        <main>
-          <Switch>
-            <Route exact path={HOME_PAGE_URL} component={HomePage} />
-            <Route exact path={ORDER_PAGE_URL} component={OrderPage} />
-            <Route exact path={LOGIN_REDIRECT_RUL} component={LoginRedirect} />
-            <Route path={`${ORDER_STATUS_PAGE_URL}/:orderId`} component={OrderStatusPage} />
-            <Route render={() => <p>Not Fount!</p>} />
-          </Switch>
-        </main>
-      </div>
-    </Router>
-  </MuiThemeProvider>
-);
+/**
+ * The root component that contains the theme, routers, navbar, and login dialog
+ */
+export class App extends Component {
+  static propTypes = {
+    user: PropTypes.object.isRequired,
+    parserUserFromJwt: PropTypes.func.isRequired
+  };
 
-export default App;
+  /**
+   * If Redux does not have user and local storage has jwt, use jwt to parser the user.
+   * @param {object} props contains the component's prop value.
+   * @return {null} No return.
+   */
+  constructor(props) {
+    super(props);
+    if (!props.user._id) {
+      const jwtMessage = localStorage.getItem(JWT_MESSAGE);
+      if (jwtMessage) props.parserUserFromJwt(jwtMessage);
+    }
+  }
+
+  state = {
+    isLoginDialogOpen: false,
+    isLogoutSnackBarOpen: false,
+    isLoginSnackbarOpen: false,
+    snackbarMessage: ''
+  };
+
+  /**
+   * Setting the open state to an opposite value.
+   * @return {null} No return.
+   */
+  handleToggleDialog = () => this.setState(({ isLoginDialogOpen }) => ({ isLoginDialogOpen: !isLoginDialogOpen }));
+
+  /**
+   * Setting the snackbarOpen state to true in order to open the snackbar.
+   * If message paramter is a string, set it to message state. Otherwise, set a empty message.
+   * @param {string} message will be showed in the snackbar.
+   * @return {null} No return.
+   */
+  handleToggleSnackbar = message => this.setState(({ isLoginSnackbarOpen }) =>
+    ({ isLoginSnackbarOpen: !isLoginSnackbarOpen, snackbarMessage: typeof message === 'string' ? message : '' }));
+
+  /**
+   * Set the logoutSnackbarOpen state to an opposite value.
+   * @return {null} No return.
+   */
+  handleToggleLogoutSnackbar = () => this.setState(({ isLogoutSnackBarOpen }) =>
+    ({ isLogoutSnackBarOpen: !isLogoutSnackBarOpen }));
+
+  /**
+   * It will be pass down in a context in order to make it available for all component.
+   * This method will be used when logout action is performed.
+   * Clear the state isLoginDialogOpen in order to prevent the LoginDialog shows up.
+   * @return {null} No return.
+   */
+  handleLogoutAction = () => this.setState({ isLoginDialogOpen: false, isLogoutSnackBarOpen: true });
+
+  /**
+   * The render method for the component
+   * @return {jsx} Return jsx
+   */
+  render() {
+    const { user } = this.props;
+    const { isLogoutSnackBarOpen, isLoginDialogOpen, isLoginSnackbarOpen, snackbarMessage } = this.state;
+    return (
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          {/* Using a context to give children components access to login dialog */}
+          <LoginDialogContext.Provider value={{ handleLogoutAction: this.handleLogoutAction, handleToggleLoginDialog: this.handleToggleDialog }}>
+            <div>
+              <CssBaseline />
+              <Navbar />
+              <main>
+                <Switch>
+                  <Route exact path={HOME_PAGE_URL} component={HomePage} />
+                  <Route exact path={ORDER_PAGE_URL} component={OrderPage} />
+                  <Route exact path={LOGIN_REDIRECT_RUL} component={LoginRedirect} />
+                  <Route path={`${ORDER_STATUS_PAGE_URL}/:orderId`} component={OrderStatusPage} />
+                  <Route render={() => <p>Not Fount!</p>} />
+                </Switch>
+              </main>
+              {!user._id && ( // If a user has already login, do not need to load componnets below.
+                <Fragment>
+                  <LoginDialog open={isLoginDialogOpen} onClose={this.handleToggleDialog} onToggleSnackbar={this.handleToggleSnackbar} />
+                  <LoginDialogSnackbar open={isLoginSnackbarOpen} onClose={this.handleToggleSnackbar} message={snackbarMessage} />
+                </Fragment>
+              )}
+              <LogoutSnackbar open={isLogoutSnackBarOpen} onClose={this.handleToggleLogoutSnackbar} />
+            </div>
+          </LoginDialogContext.Provider>
+        </Router>
+      </MuiThemeProvider>
+    );
+  }
+}
+/* istanbul ignore next */
+const mapStateToProps = state => ({
+  user: state.user
+});
+/* istanbul ignore next */
+const mapDispatchToProps = dispatch => ({
+  parserUserFromJwt: jwtMessage => dispatch(parserUserFromJwt(jwtMessage))
+});
+export default connect(mapStateToProps, mapDispatchToProps)(App);
