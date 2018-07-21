7bae38eaab5e7575aeeb54bc1b3aac5cc6d51fcc
Kevin W <rocwang0316@gmail.com>
2018-07-21 14:09:03 -0700

    Changed the amount's inital state to null in order to do the initialization check.

--- app/reducers/OrdersReducers.js
+++ app/reducers/OrdersReducers.js
@@ -4,11 +4,12 @@ import { INCREASE_ORDER_AMOUNT_SUCCESS, FETCH_ORDER_AMOUNT_SUCCESS } from '../ac
  * The Redux state.
  * For right now, just amount is needed. But in the future we can put more information if we need to optimize the database call.
  * This state can be used as a cache. So, I keep it as an object.
+ * The amount is setting to null in order to check whether the fetchOrderAmount has already been called.
  * @param {object} state contain all value
  * @param {object} action has action values
  * @return {object} return new state
  */
-const orders = (/* istanbul ignore next */state = { amount: 0 }, { type, amount }) => {
+const orders = (/* istanbul ignore next */state = { amount: null }, { type, amount }) => {
   switch (type) {
     case INCREASE_ORDER_AMOUNT_SUCCESS:
       return { ...state, amount: state.amount + 1 };
