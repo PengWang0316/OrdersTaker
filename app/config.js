0a293727914f887b58717112a7518176e583d048
Kevin W <rocwang0316@gmail.com>
2018-07-21 16:37:07 -0700

    Added the MAX_ORDER_AMOUNT variable.

--- app/config.js
+++ app/config.js
@@ -10,6 +10,7 @@ export const CLOUDINARY_UPLOAD_PRESET = 'OrdersTakerWebApp'; // The preset of Cl
 export const LOGIN_CALLBACK_URL = 'OrdersTaker_loginCallbackUrl';
 
 export const ITEM_ONE_PRICE_KEY = '_onePrice';
+export const MAX_ORDER_AMOUNT = 10; // How many orders will show in the one page
 
 /* URLS */
 export const HOME_PAGE_URL = '/';
