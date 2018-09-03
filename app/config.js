8fefeca1017fe82679911fa190dc0ccefc14bb41
Kevin W <rocwang0316@gmail.com>
2018-09-02 23:03:21 -0700

    Added the test ports.

--- app/config.js
+++ app/config.js
@@ -3,11 +3,11 @@ export const ADMINISTRATOR_ROLE = 1;
 export const SUPER_USER_ROLE = 2;
 export const NORMAL_USER_ROLE = 3;
 // export const BASE_URL = 'https://orders-taker.glitch.me'; // Development server
-export const BASE_URL = 'https://orderstaker.kevin-project.com:8080'; // Production server
-// export const NUMBER_OF_READING_PER_PAGE_RECENT_READINGS = 5; // How many reading will show on the recent readings page.
-// export const NUMBER_OF_USER_PER_PAGE = 30; // How many user will be showed in the all user list.
-// export const NUMBER_OF_READING_PER_PAGE = 10; // How many reading will be showed in one page.
-// export const NUMBER_PER_PAGE_JOURNAL = 5; // How many journal will be showed in all journal page.
+export const BASE_URL = 'https://orderstaker.kevin-project.com:8081'; // Development server on the AWS
+// export const BASE_URL = 'https://orderstaker.kevin-project.com:8080'; // Production server
+export const SOCKETIO_URL = 'https://orderstaker.kevin-project.com:3001';
+// export const SOCKETIO_URL = 'https://orderstaker.kevin-project.com:3000'; // Production server
+
 export const CLOUDINARY_UPLOAD_PRESET = 'OrdersTakerWebApp'; // The preset of Cloudinary.
 export const LOGIN_CALLBACK_URL = 'OrdersTaker_loginCallbackUrl';
 
