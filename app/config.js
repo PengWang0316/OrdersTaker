8a116db54975adf138b0a27b809bfaf96d844f68
Kevin W <rocwang0316@gmail.com>
2018-09-09 16:14:26 -0700

    Switched back to the production mode.

--- app/config.js
+++ app/config.js
@@ -3,10 +3,10 @@ export const ADMINISTRATOR_ROLE = 1;
 export const SUPER_USER_ROLE = 2;
 export const NORMAL_USER_ROLE = 3;
 // export const BASE_URL = 'https://orders-taker.glitch.me'; // Development server
-export const BASE_URL = 'https://orderstaker.kevin-project.com:8081'; // Development server on the AWS
-// export const BASE_URL = 'https://orderstaker.kevin-project.com:8080'; // Production server
-export const SOCKETIO_URL = 'https://orderstaker.kevin-project.com:3001';
-// export const SOCKETIO_URL = 'https://orderstaker.kevin-project.com:3000'; // Production server
+// export const BASE_URL = 'https://orderstaker.kevin-project.com:8081'; // Development server on the AWS
+export const BASE_URL = 'https://orderstaker.kevin-project.com:8080'; // Production server
+// export const SOCKETIO_URL = 'https://orderstaker.kevin-project.com:3001';
+export const SOCKETIO_URL = 'https://orderstaker.kevin-project.com:3000'; // Production server
 
 export const CLOUDINARY_UPLOAD_PRESET = 'OrdersTakerWebApp'; // The preset of Cloudinary.
 export const LOGIN_CALLBACK_URL = 'OrdersTaker_loginCallbackUrl';
